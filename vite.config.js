8e6277652aacc56268cfe0e1056bd88dd17d2c23
ncwjsp <nc.wjsp@gmail.com>
2024-08-16 14:26:50 +0700

    fix: base path problem in vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/routing-app/",
-});
+  base: command === "serve" ? "/" : "/routing-app/",
+}));
